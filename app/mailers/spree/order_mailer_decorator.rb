ee9299a3c8dedb8ae807a9506efee5cc35cf6120
Ben Radler <ben@benradler.com>
2014-04-14 20:42:50 -0700

    Add spree base helper module for 2.2.0

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,3 +1,4 @@
 Spree::OrderMailer.class_eval do
+  add_template_helper Spree::BaseHelper
   layout 'spree/layouts/email'
 end
--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,3 +1,4 @@
 Spree::UserMailer.class_eval do
+  add_template_helper Spree::BaseHelper
   layout 'spree/layouts/email'
 end
